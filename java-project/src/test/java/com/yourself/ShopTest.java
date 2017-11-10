f8213ba167f30b0e4d86caa85f69fc6b37434186
Bary Levi <barylevi@gmail.com>
2017-11-10 17:01:23 +0200

    r

--- java-project/src/test/java/com/yourself/ShopTest.java
+++ java-project/src/test/java/com/yourself/ShopTest.java
@@ -27,15 +27,12 @@ public class ShopTest {
 
     @Test
     public void test(){
-        try {
-            Shop.shop(50);
-            Assert.assertTrue("בננה לא נקנתה כאשר money = 50", outContent.toString().contains("banana"));
-            Shop.shop(10);
-            Assert.assertEquals("הודפסה הודעה למסך, למרות שאין ללקוח מספיק כסף", "", outContent.toString());
-            TechIOUtil.success(true);
-        } catch (AssertionError ae) {
-            TechIOUtil.msg("Oops! 🐞", ae.getMessage());
-            TechIOUtil.success(false);
-        }
+
+        Shop.shop(50);
+        Assert.assertTrue("בננה לא נקנתה כאשר money = 50", outContent.toString().contains("banana"));
+        Shop.shop(10);
+        Assert.assertEquals("הודפסה הודעה למסך, למרות שאין ללקוח מספיק כסף", "", outContent.toString());
+        TechIOUtil.success(true);
+
     }
 }
