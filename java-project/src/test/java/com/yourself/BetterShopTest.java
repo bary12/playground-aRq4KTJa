9ac90c383d445889bc2814bc40cb373a8b46853f
Bary Levi <barylevi@gmail.com>
2017-11-13 21:17:56 +0200

    reunite to one test

--- java-project/src/test/java/com/yourself/BetterShopTest.java
+++ java-project/src/test/java/com/yourself/BetterShopTest.java
@@ -5,30 +5,19 @@ import org.junit.Test;
 
 public class BetterShopTest {
 
+    private static final String enough = "can buy pizza";
+    private static final String notEnough = "t have enough money";
+
+
     @Test
     public void test(){
-        try {
-            assertEnough();
-            assertNotEnough();
-            Util.success(true);
-        } catch (AssertionError ae) {
-            Util.success(false);
-            Util.msg("שגיאה!", ae.getMessage());
-        }
-
-
-    }
-
-    private void assertEnough() throws AssertionError {
         String content = Util.getFromStdOut(BetterShop::shop, 25);
-        Assert.assertTrue("ההודעה \nYou can buy pizza \nלא הודפסה למסך, למרות שיש ללקוח מספיק כסף", content.contains("can buy pizza"));
-        Assert.assertFalse("הודפסה ההודעה הלא נכונה", content.contains("t have enough money"));
-    }
-
-    private void assertNotEnough() throws AssertionError {
-        String content = Util.getFromStdOut(BetterShop::shop, 15);
-        Assert.assertTrue("ההודעה \nYou don't have enough money\n לא הודפסה למסך, למרות שאין ללקוח מספיק כסף", content.contains("t have enough money"));
-        Assert.assertFalse("הודפסה ההודעה\n You can buy pizza\nכאשר לא היה ללקוח מספיק כסף", content.contains("can buy pizza"));
+        Util.assertTrue(content.contains(enough), "ההודעה \nYou can buy pizza \nלא הודפסה למסך, למרות שיש ללקוח מספיק כסף");
+        Util.assertTrue(!content.contains(notEnough), "הודפסה ההודעה הלא נכונה");
+        
+        content = Util.getFromStdOut(BetterShop::shop, 15);
+        Util.assertTrue(content.contains(enough), "ההודעה \nYou don't have enough money\n לא הודפסה למסך, למרות שאין ללקוח מספיק כסף");
+        Util.assertTrue(!content.contains(notEnough), "הודפסה ההודעה\n You can buy pizza\nכאשר לא היה ללקוח מספיק כסף");
     }
 
 }
--- java-project/src/test/java/com/yourself/ShopTest.java
+++ java-project/src/test/java/com/yourself/ShopTest.java
@@ -5,14 +5,11 @@ import org.junit.Test;
 public class ShopTest {
 
     @Test
-    public void testNotEnough(){
+    public void test(){
         String content = Util.getFromStdOut(Shop::shop, 15);
         Util.assertTrue(!content.contains("pizza"), " הלקוח הצליח לקנות את הפיצה, למרות שאין לו מספיק כסף (15)");
-    }
 
-    @Test
-    public void testEnough(){
-        String content = Util.getFromStdOut(Shop::shop, 25);
+        content = Util.getFromStdOut(Shop::shop, 25);
         Util.assertTrue(content.contains("pizza"), "הלקוח לא הצליח לקנות את הפיצה, למרות שיש לו מספיק כסף (25)");
     }
 
